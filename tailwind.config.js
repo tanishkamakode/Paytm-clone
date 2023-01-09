42944433f37d92002bb1d86edc9d73b3fdf97bbf
Tanishka <makodetanishka@gmail.com>
2023-01-09 23:56:43 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      Roboto: ["Roboto", "sans - serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
